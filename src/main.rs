f00604a232a580c3ef7101a3df3d0676ea654f30
George Madrid <gmadrid@gmail.com>
2020-07-26 20:24:54 -0400

    Added some docstrings. Also cleanup up code to remove a bunch of duplicate code and some awkward APIs.

--- src/main.rs
+++ src/main.rs
@@ -4,59 +4,60 @@ use fehler::throws;
 use rand::{thread_rng, Rng};
 use std::io::{BufRead, BufReader, Read};
 
-/// Foobar
+/// Implementation of Knuth's algorithm to pick n items from M,
+/// each with identical likelihood of inclusion.
+/// Uses O(n) space.
 #[derive(FromArgs)]
 struct PickerArgs {
-    /// quux
+    /// the number of items to pick
     #[argh(option, short = 'n', default = "1")]
     num: usize,
 
-    /// baz
+    /// print with zeros, '\0', between lines instead of '\n'.
+    /// This is useful when piping lines that contain spaces.
     #[argh(switch, short = '0')]
     print0: bool,
 
-    /// filename
+    /// the file to read. If not provided, uses standard input.
     #[argh(positional)]
     filename: Option<String>,
 }
 
 #[derive(Debug)]
-struct Picker<R>
-where
-    R: Read,
-{
+struct Picker {
+    // The number of lines read so far.
+    // Used in the calculation 1/lines_read which is the probability of picking any new line.
     lines_read: usize,
+
+    // The total number of items we wish to pick.
     num_to_choose: usize,
+
+    // The lines from the input which have already been chosen.
     chosen: Vec<String>,
-    bufreader: Option<BufReader<R>>,
+    // TODO: get rid of this!
+    //bufreader: Option<BufReader<R>>,
 }
 
-impl<R> Picker<R>
-where
-    R: Read,
-{
-    pub fn new(read: R, num: usize) -> Picker<R> {
-        let bufreader = BufReader::new(read);
+impl Picker {
+    pub fn new(num_to_choose: usize) -> Picker {
         let mut chosen = Vec::default();
-        chosen.reserve(num);
+        chosen.reserve(num_to_choose);
         Picker {
             lines_read: 0,
-            num_to_choose: num,
+            num_to_choose,
             chosen,
-            bufreader: Some(bufreader),
         }
     }
 
     #[throws]
-    pub fn pick(&mut self) {
+    pub fn pick(&mut self, reader: impl Read) {
         let mut rng = thread_rng();
-        if let Some(bufreader) = self.bufreader.take() {
-            for line in bufreader.lines() {
-                if let Ok(l) = line {
-                    self.process_line(l, &mut rng)?;
-                } else {
-                    line.with_context(|| "Error reading line.".to_string())?;
-                }
+        let bufreader = BufReader::new(reader);
+        for line in bufreader.lines() {
+            if let Ok(l) = line {
+                self.process_line(l, &mut rng)?;
+            } else {
+                line.with_context(|| "Error reading line.".to_string())?;
             }
         }
     }
@@ -91,24 +92,21 @@ where
     }
 }
 
-fn pick(filename: String, num: usize, print0: bool) -> Result<()> {
-    let file = std::fs::File::open(&filename)
-        .with_context(|| format!("Failed to open file at: {}", filename))?;
-    let bufread = BufReader::new(file);
-    let mut picker = Picker::new(bufread, num);
-    picker.pick()?;
+#[throws]
+fn pick_and_spew(r: impl Read, num: usize, print0: bool) {
+    let mut picker = Picker::new(num);
+    picker.pick(r)?;
     picker.spew(print0);
-    Ok(())
 }
 
 fn main() -> Result<()> {
     let args: PickerArgs = argh::from_env();
     if let Some(filename) = args.filename {
-        pick(filename, args.num, args.print0)?;
+        let file = std::fs::File::open(&filename)
+            .with_context(|| format!("Failed to open file at: {}", filename))?;
+        pick_and_spew(file, args.num, args.print0)?;
     } else {
-        let mut picker = Picker::new(std::io::stdin(), args.num);
-        picker.pick()?;
-        picker.spew(args.print0);
+        pick_and_spew(std::io::stdin(), args.num, args.print0)?;
     }
     Ok(())
 }
